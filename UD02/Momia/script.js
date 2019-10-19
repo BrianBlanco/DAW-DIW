8d4715628128be3b03d2677a438e94320eed4cda
Brian Giannoni Lillo <briangiannoni@gmail.com>
2019-10-19 05:36:13 +0200

    rodeadoComprobado

--- UD02/Momia/script.js
+++ UD02/Momia/script.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     crearMapa(23, 16);
 };
 
@@ -6,6 +6,8 @@ function crearMapa(ancho, alto) {
     var listaObjetos = ['llave', 'pergamino', 'momia', 'nada', 'urna'];
 
     var contador = 0;
+
+    var contenedorTotal = document.getElementById("contenedorTotal");
 
     for (let i = 0; i < alto; i++) {
         for (let j = 0; j < ancho; j++) {
@@ -21,7 +23,7 @@ function crearMapa(ancho, alto) {
                     divCuadricula.classList.add("cuadriculaExterior");
                 }
 
-            } else if (i == 1 || i == (alto - 1)|| j == 1 || j == (ancho - 2)) {
+            } else if (i == 1 || i == (alto - 1) || j == 1 || j == (ancho - 2)) {
                 divCuadricula.classList.add("pasadizo");
 
             } else if ((i + 1) % 3 == 0 || (j - 1) % 4 == 0) {
@@ -32,7 +34,7 @@ function crearMapa(ancho, alto) {
             }
 
             //divCuadricula.innerHTML = i + " - " + j;
-            document.getElementById("contenedorTotal").appendChild(divCuadricula);
+            contenedorTotal.appendChild(divCuadricula);
             contador++;
         }
     }
@@ -46,7 +48,7 @@ function moverPersonaje(posicionNuevoDiv) {
     let divNuevoClass = divNuevo.getAttribute("class");
 
     //console.log(divNuevoClass);
-    if(divNuevoClass == "pasadizo") {
+    if (divNuevoClass == "pasadizo") {
         personaje.classList.replace("personaje", "huellas");
         divNuevo.classList.replace("pasadizo", "personaje");
 
@@ -55,10 +57,11 @@ function moverPersonaje(posicionNuevoDiv) {
         divNuevo.classList.replace("huellas", "personaje");
     }
 
+    comprobarColumnaCubierta();
 
 }
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
 
     // Cogemos el valor de la tecla pulsada
     let teclaPulsada = event.key;
@@ -85,3 +88,47 @@ document.addEventListener('keydown', function(event) {
 function detectarPosicionPersonaje() {
     return document.querySelector(".personaje");
 }
+
+function comprobarColumnaCubierta() {
+    let bloques = document.querySelectorAll(".bloque");
+    var arrayContadorBloquesRellenados = new Array();
+
+    let dataIndice;
+    for (let i = 0; i < bloques.length; i++) {
+        dataIndice = parseInt(bloques[i].getAttribute("data-indice"));
+        //console.log(dataIndice);
+        if (
+            !contenedorTotal.childNodes[dataIndice - 23].classList.value.includes("pasadizo") &&
+            !contenedorTotal.childNodes[dataIndice - 1].classList.value.includes("pasadizo") &&
+            !contenedorTotal.childNodes[dataIndice + 1].classList.value.includes("pasadizo") &&
+            !contenedorTotal.childNodes[dataIndice + 23].classList.value.includes("pasadizo")
+        ) {
+
+            bloques[i].classList.add(".bloqueRodeado");
+            //console.log("bloque rellenado: " + bloques[i].getAttribute("data-indice"));
+        }
+
+        
+            //console.log("bloque rellenado: " + bloques[i].getAttribute("data-indice"));
+
+            if (
+                bloques[i].classList.value.includes("bloqueRodeado") &&
+                bloques[i + 1].classList.value.includes("bloqueRodeado") &&
+                bloques[i + 2].classList.value.includes("bloqueRodeado") &&
+                bloques[i + 15].classList.value.includes("bloqueRodeado") &&
+                bloques[i + 16].classList.value.includes("bloqueRodeado") &&
+                bloques[i + 17].classList.value.includes("bloqueRodeado")
+            ) {
+                bloques[i].classList.add("bloqueAlDescubierto");
+                bloques[i + 1].classList.add("bloqueAlDescubierto");
+                bloques[i + 2].classList.add("bloqueAlDescubierto");
+                bloques[i + 15].classList.add("bloqueAlDescubierto");
+                bloques[i + 16].classList.add("bloqueAlDescubierto");
+                bloques[i + 17].classList.add("bloqueAlDescubierto");
+            }
+        }
+
+    
+}
+
+    //console.log(document.querySelectorAll(".bloque").querySelector("data-indice"));
