9bd4327c6cb047d3161c68ffef48d1d8775ff967
BRIAN GIANNONI LILLO <briangiannoni@gmail.com>
2019-10-25 18:56:28 +0200

    Muerome

--- UD02/Momia/script.js
+++ UD02/Momia/script.js
@@ -105,7 +105,6 @@ function comprobarColumnaCubierta() {
     let dataIndice;
     for (let i = 0; i < 119; i++) {
         dataIndice = parseInt(bloques[i].getAttribute("data-indice"));
-        console.log(i);
         //console.log(dataIndice);
 
 
@@ -118,79 +117,76 @@ function comprobarColumnaCubierta() {
         //console.log(listaObjetos.includes(bloques[i].classList.value));
 
         // Me cago en mis muertos
-            //console.log("bloques: " + bloques[i].getAttribute("data-indice"));
+        //console.log("bloques: " + bloques[i].getAttribute("data-indice"));
+        if (
+            !listaObjetos.includes(bloques[i].classList.values)
+            // !listaObjetos.includes(bloques[i + 1].classList.values) &&
+            // !listaObjetos.includes(bloques[i + 2].classList.values) &&
+            // !listaObjetos.includes(bloques[i + 15].classList.values) &&
+            // !listaObjetos.includes(bloques[i + 16].classList.values) &&
+            // !listaObjetos.includes(bloques[i + 17].classList.values)
+        ) {
             if (
-                !listaObjetos.includes(bloques[i].classList.values)
-                // !listaObjetos.includes(bloques[i + 1].classList.values) &&
-                // !listaObjetos.includes(bloques[i + 2].classList.values) &&
-                // !listaObjetos.includes(bloques[i + 15].classList.values) &&
-                // !listaObjetos.includes(bloques[i + 16].classList.values) &&
-                // !listaObjetos.includes(bloques[i + 17].classList.values)
+                !contenedorTotal.childNodes[dataIndice - 23].classList.value.includes("pasadizo") &&
+                !contenedorTotal.childNodes[dataIndice - 1].classList.value.includes("pasadizo") &&
+                !contenedorTotal.childNodes[dataIndice + 1].classList.value.includes("pasadizo") &&
+                !contenedorTotal.childNodes[dataIndice + 23].classList.value.includes("pasadizo")
             ) {
-                    if (
-                        !contenedorTotal.childNodes[dataIndice - 23].classList.value.includes("pasadizo") &&
-                        !contenedorTotal.childNodes[dataIndice - 1].classList.value.includes("pasadizo") &&
-                        !contenedorTotal.childNodes[dataIndice + 1].classList.value.includes("pasadizo") &&
-                        !contenedorTotal.childNodes[dataIndice + 23].classList.value.includes("pasadizo")
-                    ) {
-                        console.log("Peta?");
-                        bloques[i].classList.add("bloqueRodeado");
-                        //console.log("Bloque rodeado: " + bloques[i].getAttribute("data-indice"));
-                        //console.log("bloque rellenado: " + bloques[i].getAttribute("data-indice"));
-                    }
+                bloques[i].classList.add("bloqueRodeado");
+                //console.log("Bloque rodeado: " + bloques[i].getAttribute("data-indice"));
+                //console.log("bloque rellenado: " + bloques[i].getAttribute("data-indice"));
             }
-            console.log("Peta?2");
+        }
 
-            //console.log("bloque rellenado: " + bloques[i].getAttribute("data-indice"));
-            if (parseInt(i / 15) % 2 == 0) {
-                if (i % 3 == 0) {
-                    console.log(bloques[i]);
-                    if (
-                        bloques[i].classList.contains("bloqueRodeado") &&
-                        bloques[i + 1].classList.contains("bloqueRodeado") &&
-                        bloques[i + 2].classList.contains("bloqueRodeado") &&
-                        bloques[i + 15].classList.contains("bloqueRodeado") &&
-                        bloques[i + 16].classList.contains("bloqueRodeado") &&
-                        bloques[i + 17].classList.contains("bloqueRodeado")
-                    ) {
-                        if (i == 0) {
-                            bloqueADescubrir = "papiro";
-                        } else if (i == 3) {
-                            bloqueADescubrir = "llave";
-                        } else if (i == 72) {
-                            bloqueADescubrir = "sarcofago";
-                        } else {
-                            bloqueADescubrir = "nada";
-                        }
-
-                        //add(bloqueADescubrir);
-                        // bloques[i].classList.add(bloqueADescubrir);
-                        // bloques[i + 1].classList.add(bloqueADescubrir);
-                        // bloques[i + 2].classList.add(bloqueADescubrir);
-                        // bloques[i + 15].classList.add(bloqueADescubrir);
-                        // bloques[i + 16].classList.add(bloqueADescubrir);
-                        // bloques[i + 17].classList.add(bloqueADescubrir);
-
-
-
-                        bloques[i].classList.replace("bloqueRodeado", bloqueADescubrir);
-                        bloques[i + 1].classList.replace("bloqueRodeado", bloqueADescubrir);
-                        bloques[i + 2].classList.replace("bloqueRodeado", bloqueADescubrir);
-                        bloques[i + 15].classList.replace("bloqueRodeado", bloqueADescubrir);
-                        bloques[i + 16].classList.replace("bloqueRodeado", bloqueADescubrir);
-                        bloques[i + 17].classList.replace("bloqueRodeado", bloqueADescubrir);
-
-
-                        //     bloques[i].classList.remove("bloqueRodeado");
-                        //     bloques[i + 1].classList.remove("bloqueRodeado");
-                        //     bloques[i + 2].classList.remove("bloqueRodeado");
-                        //     bloques[i + 15].classList.remove("bloqueRodeado");
-                        //     bloques[i + 16].classList.remove("bloqueRodeado");
-                        //     bloques[i + 17].classList.remove("bloqueRodeado");
+        //console.log("bloque rellenado: " + bloques[i].getAttribute("data-indice"));
+        if (parseInt(i / 15) % 2 == 0) {
+            if (i % 3 == 0) {
+                if (
+                    bloques[i].classList.contains("bloqueRodeado") &&
+                    bloques[i + 1].classList.contains("bloqueRodeado") &&
+                    bloques[i + 2].classList.contains("bloqueRodeado") &&
+                    bloques[i + 15].classList.contains("bloqueRodeado") &&
+                    bloques[i + 16].classList.contains("bloqueRodeado") &&
+                    bloques[i + 17].classList.contains("bloqueRodeado")
+                ) {
+                    if (i == 0) {
+                        bloqueADescubrir = "papiro";
+                    } else if (i == 3) {
+                        bloqueADescubrir = "llave";
+                    } else if (i == 72) {
+                        bloqueADescubrir = "sarcofago";
+                    } else {
+                        bloqueADescubrir = "nada";
                     }
+
+                    //add(bloqueADescubrir);
+                    // bloques[i].classList.add(bloqueADescubrir);
+                    // bloques[i + 1].classList.add(bloqueADescubrir);
+                    // bloques[i + 2].classList.add(bloqueADescubrir);
+                    // bloques[i + 15].classList.add(bloqueADescubrir);
+                    // bloques[i + 16].classList.add(bloqueADescubrir);
+                    // bloques[i + 17].classList.add(bloqueADescubrir);
+
+
+
+                    bloques[i].classList.replace("bloqueRodeado", bloqueADescubrir);
+                    bloques[i + 1].classList.replace("bloqueRodeado", bloqueADescubrir);
+                    bloques[i + 2].classList.replace("bloqueRodeado", bloqueADescubrir);
+                    bloques[i + 15].classList.replace("bloqueRodeado", bloqueADescubrir);
+                    bloques[i + 16].classList.replace("bloqueRodeado", bloqueADescubrir);
+                    bloques[i + 17].classList.replace("bloqueRodeado", bloqueADescubrir);
+
+
+                    //     bloques[i].classList.remove("bloqueRodeado");
+                    //     bloques[i + 1].classList.remove("bloqueRodeado");
+                    //     bloques[i + 2].classList.remove("bloqueRodeado");
+                    //     bloques[i + 15].classList.remove("bloqueRodeado");
+                    //     bloques[i + 16].classList.remove("bloqueRodeado");
+                    //     bloques[i + 17].classList.remove("bloqueRodeado");
                 }
             }
-        
+        }
+
     }
 
 }
@@ -207,13 +203,66 @@ function movimientoMomia() {
     // X
 
     // Comprobamos que si el personaje está a la izquierda
+
+
+    momiaSiguePersonaje(dataIndicePersonaje, dataIndiceMomia, direccionX, direccionY);
+
+
+
+    // Y
+
+
+}
+
+//Esto hace cosas
+function detectarPosicionPersonaje() {
+    return document.querySelector(".personaje");
+}
+
+//Esto hace cosas
+function detectarPosicionMomia() {
+    return document.querySelector(".momia");
+}
+
+//Esto hace s
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function momiaSiguePersonaje(dataIndicePersonaje, dataIndiceMomia, direccionX, direccionY) {
     if (parseInt(dataIndicePersonaje % 23) == parseInt(dataIndiceMomia % 23)) {
 
         // Comprobamos que si el personaje está arriba
         if (parseInt(dataIndicePersonaje % 23) < parseInt(dataIndiceMomia % 23)) {
 
-           momiaSiguePersonaje(dataIndicePersonaje, dataIndiceMomia, direccionX);
+            // Cogemos una posición a la izquierda de la momia
+            divNuevo = document.querySelector("[data-indice = '" + parseInt(dataIndiceMomia - 1) + "']");
 
+            // Si no es pasillo
+            if (!divNuevo.getAttribute("data-indice").includes("pasillo")) {
+
+                // Conseguimos el valor de una posición arriba izquierda para esquivar el bloque
+                divNuevo = document.querySelector("[data-indice = '" + parseInt(dataIndiceMomia - 1 - 23) + "']");
+
+                // Comprobamos si arriba a la izquierda hay bloque
+                if (divNuevo.classList.value.includes("pasadizo")) {
+                    detectarPosicionMomia().classList.replace("momia", "pasadizo");
+                    divNuevo.classList.replace("pasadizo", "momia");
+
+                    // De no haberlo, la momia iría abajo a la izquierda
+                } else {
+                    divNuevo = document.querySelector("[data-indice = '" + parseInt(dataIndiceMomia - 1 + 23) + "']");
+                    detectarPosicionMomia().classList.replace("momia", "pasadizo");
+                    divNuevo.classList.replace("pasadizo", "momia");
+                }
+
+                // Si es pasillo, anda hacia el personaje
+            } else {
+                direccionX = dataIndiceMomia - 1;
+            }
             // De no estar arriba de la momia
         } else {
 
@@ -224,6 +273,10 @@ function movimientoMomia() {
             if (divNuevo.classList.value.includes("pasadizo")) {
                 detectarPosicionMomia().classList.replace("momia", "pasadizo");
                 divNuevo.classList.replace("pasadizo", "momia");
+            } else if (divNuevo.classList.value.includes("huellas")) {
+                detectarPosicionMomia().classList.replace("momia", "huellas");
+                divNuevo.classList.replace("huellas", "momia");
+
 
                 // Sino, va abajo a la izquierda
             } else {
@@ -242,17 +295,12 @@ function movimientoMomia() {
         direccionX = dataIndiceMomia + 1;
     }
 
-
-
-
-
     // Y
     if (parseInt(dataIndicePersonaje / 23) == parseInt(dataIndiceMomia / 23)) {
         if (parseInt(dataIndicePersonaje % 23) < parseInt(dataIndiceMomia % 23)) {
             divNuevo = document.querySelector("[data-indice = '" + parseInt(dataIndiceMomia - 1) + "']");
             if (!divNuevo.classList.value.includes("pasadizo")) {
                 divNuevo = document.querySelector("[data-indice = '" + parseInt(dataIndiceMomia - 1 - 23) + "']");
-                console.log("syso");
                 if (divNuevo.classList.value.includes("pasadizo")) {
 
                     detectarPosicionMomia().classList.replace("momia", "pasadizo");
@@ -277,9 +325,6 @@ function movimientoMomia() {
                 divNuevo.classList.replace("pasadizo", "momia");
             }
         }
-
-
-
     } else if (parseInt(dataIndicePersonaje / 23) < parseInt(dataIndiceMomia / 23)) {
         direccionY = dataIndiceMomia - 23;
     } else {
@@ -300,52 +345,6 @@ function movimientoMomia() {
         divNuevo = document.querySelector("[data-indice = '" + direccionY + "']");
     }
 
-}
-
-//Esto hace cosas
-function detectarPosicionPersonaje() {
-    return document.querySelector(".personaje");
-}
-
-//Esto hace cosas
-function detectarPosicionMomia() {
-    return document.querySelector(".momia");
-}
-
-//Esto hace s
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-function momiaSiguePersonaje(dataIndicePersonaje, dataIndiceMomia, direccionX, horizontal, vertical) {
-     // Cogemos una posición a la izquierda de la momia
-     divNuevo = document.querySelector("[data-indice = '" + parseInt(dataIndiceMomia - 1) + "']");
-
-     // Si no es pasillo
-     if (!divNuevo.getAttribute("data-indice").includes("pasillo")) {
-
-         // Conseguimos el valor de una posición arriba izquierda para esquivar el bloque
-         divNuevo = document.querySelector("[data-indice = '" + parseInt(dataIndiceMomia - 1 - 23) + "']");
-
-         // Comprobamos si arriba a la izquierda hay bloque
-         if (divNuevo.classList.value.includes("pasadizo")) {
-             detectarPosicionMomia().classList.replace("momia", "pasadizo");
-             divNuevo.classList.replace("pasadizo", "momia");
-
-             // De no haberlo, la momia iría abajo a la izquierda
-         } else {
-             divNuevo = document.querySelector("[data-indice = '" + parseInt(dataIndiceMomia - 1 + 23) + "']");
-             detectarPosicionMomia().classList.replace("momia", "pasadizo");
-             divNuevo.classList.replace("pasadizo", "momia");
-         }
-
-         // Si es pasillo, anda hacia el personaje
-     } else {
-         direccionX = dataIndiceMomia - 1;
-     }
 }
 /*
 let bloques = document.querySelectorAll(".bloque");
