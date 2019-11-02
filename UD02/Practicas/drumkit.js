330d9c683e98b3df0c9c7f426b5e9722938c8d9e
Brian Giannoni Lillo <briangiannoni@gmail.com>
2019-11-02 20:44:27 +0100

    acabado ejericico DrumKit

--- UD02/Practicas/drumkit.js
+++ UD02/Practicas/drumkit.js
@@ -1,1 +1,24 @@
-document.addEventListener("okeypress");
+document.addEventListener("keypress", botonApretado);
+
+const key = document.querySelectorAll("div");
+key.forEach(boton => { boton.addEventListener('transitionend', borrarTransicion); });
+
+function botonApretado(e) {
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio) {
+        return;
+    }
+
+    audio.play();
+    audio.currentTime = 0;
+
+    const teclaPulsada = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    teclaPulsada.classList.add("sombra");
+
+
+}
+
+function borrarTransicion() {
+    this.classList.remove("sombra");
+}
