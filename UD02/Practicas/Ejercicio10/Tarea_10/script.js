7b197b7e1e1eb35967a30f94adc1d6522016d9d2
Brian Giannoni Lillo <briangiannoni@gmail.com>
2019-11-09 23:59:02 +0100

    Acabada Tarea_10

---
+++ UD02/Practicas/Ejercicio10/Tarea_10/script.js
@@ -0,0 +1,15 @@
+function ponerClasesALasBestias() {
+  cajas = document.getElementsByClassName('caja');
+
+  for (let i = 0; i < cajas.length; i++) {
+    cajas[i].classList.toggle("nuevaPosicion");
+  }
+
+}
+
+function init() {
+  let button = document.querySelector('button');
+  button.addEventListener('click', ponerClasesALasBestias);
+}
+
+window.onload = init;
