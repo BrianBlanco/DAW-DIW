664da1b45a54192682f3299a9963bff4447a2016
brianWP <briangiannoni@gmail.com>
2020-01-20 16:37:51 +0100

    gracias angel

--- UD04/Canvas/Tarea01-Graficos/graficos.js
+++ UD04/Canvas/Tarea01-Graficos/graficos.js
@@ -1,16 +1,42 @@
-function buildGrafico(){
-    console.info(" * Construyendo grafico ");
+function buildGrafico() {
+    let inicioAngulo = -Math.PI/2;
+    let finAngulo = 0;
+
+    let claves = document.querySelectorAll("input[class='left']");
+    let valores = document.querySelectorAll("input[class='right']");
+
+    const canvas = document.querySelector('canvas');
+    let ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'purple';
+  
+
+    for (let i = 0; i < claves.length; i++) {
+        ctx.beginPath();
+        ctx.moveTo(200, 200);
+        finAngulo = inicioAngulo + (parseInt(valores[i].value)/18)*2*Math.PI;
+        console.log(valores[i]);
+        ctx.arc(200, 200, 100, inicioAngulo,finAngulo);
+        ctx.moveTo(200, 200);
+        ctx.stroke();
+
+         inicioAngulo = finAngulo;
+    }
+
+
+
+
+
 }
 
-function loadListeners(){
+function loadListeners() {
     document.querySelector("input[name='grafiqueame']").addEventListener("click", buildGrafico);
 }
 
 
-function init(){
+function init() {
     console.log(" * Init ");
     loadListeners();
-    
+
 }
 
-window.onload=init;
+window.onload = init;
